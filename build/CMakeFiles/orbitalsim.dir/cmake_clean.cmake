0bbe0c231ff41a58a7d3119c041316f05709fa6e
leicasais <lcasais@itba.edu.ar>
2025-08-25 19:29:31 -0300

    pusheo inicializacion

---
+++ build/CMakeFiles/orbitalsim.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orbitalsim.dir/main.cpp.o"
+  "CMakeFiles/orbitalsim.dir/main.cpp.o.d"
+  "CMakeFiles/orbitalsim.dir/orbitalSim.cpp.o"
+  "CMakeFiles/orbitalsim.dir/orbitalSim.cpp.o.d"
+  "CMakeFiles/orbitalsim.dir/view.cpp.o"
+  "CMakeFiles/orbitalsim.dir/view.cpp.o.d"
+  "orbitalsim"
+  "orbitalsim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orbitalsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/orbitalsim.dir/main.cpp.o.d
@@ -0,0 +1,7 @@
+CMakeFiles/orbitalsim.dir/main.cpp.o: \
+ /home/leila/Documents/Algoritmos/1.\ Warm\ Up\ -\ Starter\ Code/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/leila/Documents/Algoritmos/1.\ Warm\ Up\ -\ Starter\ Code/orbitalSim.h \
+ /home/leila/Documents/Algoritmos/1.\ Warm\ Up\ -\ Starter\ Code/view.h \
+ /home/leila/dev/vcpkg/installed/x64-linux/include/raylib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stdarg.h
